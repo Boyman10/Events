d062c5ddab91d40a35b1bc00714fac2b4df8b990
Boyman10 <contact@yasi.fr>
2018-12-11 22:19:50 +0100

    Fully working example using kafka and sending/receiving payload for topic 1

--- src/main/java/com/example/events/EventsApplication.java
+++ src/main/java/com/example/events/EventsApplication.java
@@ -1,13 +1,22 @@
 package com.example.events;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventsApplication {
+public class EventsApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(EventsApplication.class, args);
     }
 
+    @Autowired
+    private KafkaSender sender;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        sender.send("topic1","Spring Kafka Producer and Consumer Example");
+    }
 }
