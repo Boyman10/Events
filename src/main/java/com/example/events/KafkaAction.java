fe1505a0c14d514a4bbd374835d30e1284c605ca
Boyman10 <contact@yasi.fr>
2018-12-11 22:10:12 +0100

    Preparing simple example of using kafka - consumer/receiver & sender/producer with their respective config

---
+++ src/main/java/com/example/events/KafkaAction.java
@@ -0,0 +1,19 @@
+package com.example.events;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaAction {
+    @Autowired
+    KafkaSender kafkaSender;
+
+    @PostMapping("/kafka/{topicName}")
+    public String sendToTopic(@PathVariable String topicName, @RequestBody String message) {
+        kafkaSender.send(topicName, message);
+        return "Message sent";
+    }
+}
